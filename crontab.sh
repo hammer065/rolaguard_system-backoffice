baf63313438d7392cd4e10fdc6e184eba172ce44
cyones <cristianyones@argeniss.com>
2020-05-11 15:22:27 -0300

    Configured system cron to run db jobs

--- crontab.sh
+++ crontab.sh
@@ -3,18 +3,22 @@
 # Ensure the log file exists
 touch /iot_home/crontab.log
 
+echo "DB_HOST=$DB_HOST" > /etc/crontab
+echo "DB_PORT=$DB_PORT" >> /etc/crontab
+echo "DB_NAME=$DB_NAME" >> /etc/crontab
+echo "DB_USERNAME=$DB_USERNAME" >> /etc/crontab
+echo "DB_PASSWORD=$DB_PASSWORD" >> /etc/crontab
+
 # Added cronjobs in a new crontab
 # Run periodically
-echo '0 23 * * 5 python pg_admin.py scripts/sql/db_cleanup_packet.sql >> /iot_home/crontab.log 2>$1' > /etc/crontab
-echo '15 * * * * python pg_admin.py scripts/sql/db_health_status.sql >> /iot_home/crontab.log 2>$1' >> /etc/crontab
-echo '*/5 * * * * python pg_admin.py scripts/sql/db_sp_populate_stats_counters.sql >> /iot_home/crontab.log 2>$1' >> /etc/crontab
-echo '15,45 * * * * python pg_admin.py scripts/sql/db_resolve_quarantine_timeout.sql >> /iot_home/crontab.log 2>$1' >> /etc/crontab
+echo '0 23 * * 5 /usr/local/bin/python /iot_home/pg_admin.py /iot_home/scripts/sql/db_cleanup_packet.sql >> /iot_home/crontab.log' >> /etc/crontab
+echo '15 * * * * /usr/local/bin/python /iot_home/pg_admin.py /iot_home/scripts/sql/db_health_status.sql >> /iot_home/crontab.log' >> /etc/crontab
+echo '*/5 * * * * /usr/local/bin/python /iot_home/pg_admin.py /iot_home/scripts/sql/db_sp_populate_stats_counters.sql >> /iot_home/crontab.log' >> /etc/crontab
+echo '15,45 * * * * /usr/local/bin/python /iot_home/pg_admin.py /iot_home/scripts/sql/db_resolve_quarantine_timeout.sql >> /iot_home/crontab.log' >> /etc/crontab
 
-# Registering the new crontab
-crontab /etc/crontab
-
+crontab -u root /etc/crontab
 # Starting the cron
-/usr/sbin/service cron start
+service cron start
 
 # Displaying logs
 tail -F /iot_home/crontab.log

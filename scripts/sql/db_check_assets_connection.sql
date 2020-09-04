146dee41fb94a467721f1c5576d5c93ae9d08b7d
Luis Santiago Re <lsantire2@gmail.com>
2020-09-04 14:04:54 -0300

    Added some needed data when creating not transmitting alerts (#6)

--- scripts/sql/db_check_assets_connection.sql
+++ scripts/sql/db_check_assets_connection.sql
@@ -14,11 +14,16 @@ SELECT
     (pitem.enabled and dc.deleted_at is null and dc.status = 'CONNECTED'::datacollectorstatus) as should_create_alert,
     true as show,
     now() as created_at,
-    -1 as packet_id,
+    g.last_packet_id as packet_id,
     json_build_object(
-        'gw_hex_id', g.gw_hex_id,
+        'packet_id', g.last_packet_id,
+        'gateway', g.gw_hex_id,
         'gw_name', g.name,
         'gw_vendor', g.vendor,
+        'dev_eui', null,
+        'dev_name', null,
+        'dev_vendor', null,
+        'dev_addr', null,
         'last_activity', g.last_activity,
         'activity_freq', g.activity_freq,
         'policy_name', p.name,
@@ -26,7 +31,7 @@ SELECT
     ) as parameters
 INTO gw_to_disconnect
 FROM gateway g
-    JOIN data_collector dc on g.organization_id = dc.organization_id
+    JOIN data_collector dc on g.data_collector_id = dc.id
     JOIN policy_item pitem on pitem.alert_type_code = 'LAF-403' and dc.policy_id = pitem.policy_id
     JOIN policy p on p.id = pitem.policy_id
 WHERE g.connected and pitem.parameters::jsonb ? 'disconnection_sensitivity' and
@@ -50,6 +55,7 @@ WHERE id in (select gateway_id from gw_to_disconnect)
 -- 1) Get devices to disconnect and the data needed to create the corresponding alerts
 SELECT
     DISTINCT ON (d.id, dc.id)
+    g.id as gateway_id,
     d.id as device_id,
     d.organization_id as organization_id,
     dc.id as data_collector_id,
@@ -59,9 +65,14 @@ SELECT
     now() as created_at,
     d.last_packet_id as packet_id,
     json_build_object(
+        'packet_id', d.last_packet_id,
+        'gateway', g.gw_hex_id,
+        'gw_name', g.name,
+        'gw_vendor', g.vendor,
         'dev_eui', d.dev_eui,
         'dev_name', d.name,
         'dev_vendor', d.vendor,
+        'dev_addr', null,
         'last_activity', d.last_activity,
         'activity_freq', d.activity_freq,
         'policy_name', p.name,
@@ -69,16 +80,18 @@ SELECT
     ) as parameters
 INTO dv_to_disconnect
 FROM device d
-    JOIN data_collector dc on d.organization_id = dc.organization_id
+    JOIN data_collector dc on d.data_collector_id = dc.id
     JOIN policy_item pitem on pitem.alert_type_code = 'LAF-401' and dc.policy_id = pitem.policy_id
     JOIN policy p on p.id = pitem.policy_id
+    JOIN packet pck on pck.id = d.last_packet_id
+    JOIN gateway g on g.data_collector_id = dc.id and g.gw_hex_id = pck.gateway
 WHERE d.connected and pitem.parameters::jsonb ? 'disconnection_sensitivity' and
     (d.last_activity + CONCAT(GREATEST(300, COALESCE(d.activity_freq, 0)/((pitem.parameters::json->>'disconnection_sensitivity')::numeric))::text, ' seconds')::interval) < now()
 
 -- 2) Add an alert for every device that will be disconnected if corresponds
 with created_alerts as (
-    INSERT INTO alert (type, created_at, packet_id, device_id, data_collector_id, parameters, show)
-    SELECT alert_type, created_at, packet_id, device_id, data_collector_id, parameters, show
+    INSERT INTO alert (type, created_at, packet_id, device_id, gateway_id, data_collector_id, parameters, show)
+    SELECT alert_type, created_at, packet_id, device_id, gateway_id, data_collector_id, parameters, show
     FROM dv_to_disconnect
     WHERE should_create_alert
     RETURNING *

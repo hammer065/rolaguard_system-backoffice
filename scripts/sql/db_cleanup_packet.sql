945f07edf022d3766359c8d091ce2746a44df8e7
cyones <cyones77@gmail.com>
2020-09-25 12:23:52 -0300

    Delete old packets after 15 days, increased work_mem

--- scripts/sql/db_cleanup_packet.sql
+++ scripts/sql/db_cleanup_packet.sql
@@ -6,19 +6,19 @@ where c.relkind = 'r' and c.relname = 'packet';
 
 select to_char(min(date), 'YYYY-MM-DD HH12:MI:SS') as delete_from, to_char(max(date), 'YYYY-MM-DD HH12:MI:SS') as delete_to, cast(count(1) as varchar(12)) as rows
 from packet
-where date < current_date - interval '60 day'
+where date < current_date - interval '15 day'
 and id not in (select packet_id from alert);
 
 delete
 from packet
-where date < current_date - interval '60 day'
+where date < current_date - interval '15 day'
 and id not in (select packet_id from alert);
 
-set maintenance_work_mem='2 GB';
+set maintenance_work_mem='4 GB';
 
 show maintenance_work_mem;
 
-SET lock_timeout TO '30min';
+SET lock_timeout TO '60min';
 
 vacuum (verbose, full) packet;
 
